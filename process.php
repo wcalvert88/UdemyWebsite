7776be02fbbfb6a976eba436cb28a5a7bf6ad787
wcalvert88 <wcalvert88@gmail.com>
2018-04-15 12:42:14 -0400

    added php to send email

--- process.php
+++ process.php
@@ -1,6 +1,14 @@
 <?php 
 if(isset($_POST['submit'])) {
+    $to = "wcalvert88@gmail.com";
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $message = wordwrap($_POST['message'], 70);
+    $headers = "From: " . $_POST['email'] . "\r\n" . 'Content-type: text/html; charset=utf-8';
     mail($to, $subject, $message, $headers);
+    header("Location: contact.html");
+} else {
+    header("Location: contact.html");
 }
 
 ?>
